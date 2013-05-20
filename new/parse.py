be1a3cd2668569adf0470db029ba330ce75a2829
Jonathan Reeve <jon.reeve@gmail.com>
2013-05-19 21:07:49 -0400

    parsing characters from file now instead of option

--- new/parse.py
+++ new/parse.py
@@ -1,6 +1,7 @@
-import sys # library for parsing command line parametrs
+import sys # library for doing system things 
 from optparse import OptionParser #parse options from the command line!
 from lxml import etree # for parsing XML
+import re # need regex for doing fancy split
 
 #filenames=[filename for filename in sys.argv[1:]] #store filenames
 #print "Using files: " #Display filenames
@@ -9,22 +10,39 @@ from lxml import etree # for parsing XML
 #xmls=[etree.parse(file) for file in sys.argv[1:]] #open and parse files
 #print 'Files loaded successfully.'  
 
-usage = 'usage: %prog -c  character1,character2  [options] file1.xml [file2.xml]'
+usage = 'usage: %prog [options] file1.xml [file2.xml]'
 parser = OptionParser(usage)
 parser.add_option("-c", "--characters", action="store", dest="characters",         
-            help="Names (XML IDs) of the characters whose dialog you want to extract.")
+		help="A text file containing a comma-separated list of the names (i.e. XML IDs) of the characters whose dialog you want to extract. Default: characters.txt")
 parser.add_option("-v", "--verbose",
             action="store_true", dest="verbose")
 parser.add_option("-q", "--quiet",
             action="store_false", dest="verbose")
 
+#Error check TEI files
 (options, args) = parser.parse_args()
 if len(args) != 1:
     parser.error("Please specify a TEI filename.")
 
-characters=options.characters.split(',') #split up character names from input
+if not options.characters: 
+	charactersFile='characters.txt'
+else: 
+	charactersFile=options.characters
 
-#Verbose output 
+#Error check Characters file
+try: 
+	rawCharacters=open(charactersFile).read()
+except IOError: 
+	parser.error("Can't find characters.txt.") 
+
+#Parse characters file
+charactersMessy=re.split('\n|,',rawCharacters) #split by commas or newlines and strip out whitespace
+characters=[]
+for character in charactersMessy: 
+	characters.append(character.strip()) #remove whitespace
+characters=filter(None,characters) # get rid of empty characters
+
+#Verbose output will help to debug
 if options.verbose:
         print "Using these characters: "
         for character in characters:
@@ -56,5 +74,6 @@ clean=""
 for line in output: 
 	clean+=line.strip()+'\n' #strip all extra space and then make them individual lines
 
-print(clean) 
-
+#encoding it makes it work with piping as described here: http://stackoverflow.com/questions/492483/setting-the-correct-encoding-when-piping-stdout-in-python 
+encoded=clean.encode('utf-8')
+sys.stdout.write(encoded)
